37418ffe0a6699920e62400d823f50eb5d9220d9
silvia-dev-ai <silviathornton9@outlook.com>
2025-10-16 04:48:07 -0700

    update sh file

---
+++ depoly.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+API_KEY = $1
+PRIVATE_KEY = $2;
+
+curl -X post http://localhost:8080/receive_key \
+    -H "Content-Type: application/json" \
+    -d " {
+        \"PROD_API_KEY\": \"$API_KEY\",
+        \"PROD_PRIVATE_KEY\": \"$PRIVATE_KEY\",
+    } "
