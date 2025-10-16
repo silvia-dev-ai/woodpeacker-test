2358ed312ce626bfa491a3ab6e009bd4b64ff8c9
silvia-dev-ai <silviathornton9@outlook.com>
2025-10-16 05:07:55 -0700

    update command

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,12 @@
 API_KEY = $1
 PRIVATE_KEY = $2;
 
-curl -X post http://localhost:8080/receive_key \
-    -H "Content-Type: application/json" \
-    -d " {
-        \"PROD_API_KEY\": \"$API_KEY\",
-        \"PROD_PRIVATE_KEY\": \"$PRIVATE_KEY\",
-    } "
+# curl -X post http://localhost:8080/receive_key \
+#     -H "Content-Type: application/json" \
+#     -d " {
+#         \"PROD_API_KEY\": \"$API_KEY\",
+#         \"PROD_PRIVATE_KEY\": \"$PRIVATE_KEY\",
+#     } "
+
+echo $API_KEY
+echo $PRIVATE_KEY
